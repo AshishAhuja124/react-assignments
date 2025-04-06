f9bda1287b3dd6aa9eb777238e022dc6e968f1b0
Ashish Ahuja <ashishahuja124@gmail.com>
2025-04-06 16:02:26 +0530

    timer component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ImageSlider from './Components/ImageSlider'
+import Timer from './Components/Timer'
 
 function App() {
 
   return (
     <>
+      <Timer duration={2 * 24 * 60 * 60 * 1000}/>
       <ImageSlider/>
     </>
   )
