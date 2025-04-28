c9a874f2ea4c52babd751b54e332376d2e7449f9
Ashish Ahuja <ashishahuja124@gmail.com>
2025-04-28 11:53:20 +0530

    fetch posts and filter posts

--- my-app/src/Components/FetchPost.jsx
+++ my-app/src/Components/FetchPost.jsx
@@ -1,49 +1,61 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const FetchPost = () => {
   const [posts, setPosts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [searchPost, setSearchPost] = useState("");
+  const [error, setError] = useState(null);
+
 
   const fetchPosts = async () => {
     try {
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
       setPosts(res.data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
+
+  const handleSearch = (e) => {
+    console.log(e.target.value);
+    setSearchPost(e.target.value);
+  };
+
+  const filterPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(searchPost.toLowerCase())
+  );
 
   useEffect(() => {
     fetchPosts();
   }, []);
 
-  // Filter posts by title based on the search term
-  const filteredPosts = posts.filter((post) =>
-    post.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  if (loading) return <div className="p-4">Loading...</div>;
+  if (error)
+    return <div className="p-4 text-red-500">Error fetching posts.</div>;
 
   return (
     <div>
-      <h1>Posts from API</h1>
-
+      <h1>Fetching posts</h1>
       <input
         type="text"
-        placeholder="Search by title..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        style={{ marginBottom: '10px', padding: '5px' }}
+        placeholder="search post..."
+        value={searchPost}
+        onChange={handleSearch}
+        style={{ marginBottom: "10px", padding: "5px" }}
       />
 
-      <table border="1" cellPadding="5">
+      <table>
         <thead>
           <tr>
-            <th>Id</th>
+            <th>ID</th>
             <th>Title</th>
           </tr>
         </thead>
         <tbody>
-          {filteredPosts.map((post) => (
+          {filterPosts.map((post) => (
             <tr key={post.id}>
               <td>{post.id}</td>
               <td>{post.title}</td>
