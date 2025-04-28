32a1e02b91e0041e462ab7bd7f71f17b6fa4e60c
Ashish Ahuja <ashishahuja124@gmail.com>
2025-04-28 18:43:08 +0530

    debounce implement

--- my-app/src/Components/AutoComplete.jsx
+++ my-app/src/Components/AutoComplete.jsx
@@ -3,41 +3,42 @@ import React, { useEffect, useState } from "react";
 
 const AutoComplete = () => {
   const [results, setResults] = useState([]);
-  const [seachInput, setSearchInput] = useState("");
+  const [searchInput, setSearchInput] = useState("");
 
   const fetchRecipes = async () => {
     const data = await axios.get(
-      "https://dummyjson.com/recipes/search?q=" + seachInput
+      "https://dummyjson.com/recipes/search?q=" + searchInput
     );
     setResults(data?.data?.recipes);
   };
 
   useEffect(() => {
     const optimise = setTimeout(() => {
-      if(seachInput.trim()) {
-        fetchRecipes();
-
+      if (searchInput.trim()) {
+        console.log(searchInput)
+        fetchRecipes(searchInput);
       }
     }, 500);
 
-    return () => clearInterval(optimise)
-   
-  }, [seachInput]);
+    return () => clearTimeout(optimise);
+  }, [searchInput]);
 
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
   return (
     <div>
       <h1>Recipes</h1>
-
       <input
         type="text"
         placeholder="Search for recipe..."
-        value={seachInput}
+        value={searchInput} 
         onChange={(e) => setSearchInput(e.target.value)}
         className="border p-2 rounded"
       />
       <ul>
         {results.map((item) => (
-          <li>{item.name}</li>
+          <li key={item.id}>{item.name}</li>
         ))}
       </ul>
     </div>
